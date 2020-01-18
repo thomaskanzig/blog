bb4a1121a3e8074a63d95069a410513db356bd1e
Thomas Kanzig <thomas.kanzig@gmail.com>
2020-01-18 10:27:16 -0300

    Fix doc from setting controller

--- src/Controller/Admin/SettingController.php
+++ src/Controller/Admin/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Setting;
 use App\Repository\SettingRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,6 +15,12 @@ class SettingController extends AbstractController
 {
     /**
      * @Route("/admin/setting", name="admin_setting_index")
+     *
+     * @param SettingRepository $repository
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     *
+     * @return Response
      */
     public function index(SettingRepository $repository, EntityManagerInterface $em, Request $request)
     {
