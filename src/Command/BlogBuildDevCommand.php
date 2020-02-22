abd40356db05880d20a6acca19ca77b6da33ee5f
Thomas Känzig <thomas.kaenzig@massiveart.com>
2020-02-22 14:11:57 +0100

    Fix

--- src/Command/BlogBuildDevCommand.php
+++ src/Command/BlogBuildDevCommand.php
@@ -3,13 +3,9 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
-use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
