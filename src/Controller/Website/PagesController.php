885f98b83538d41fd906a806e00932c492715aa8
Thomas Känzig <thomas.kaenzig@massiveart.com>
2020-05-10 12:48:24 +0200

    Added highlight in html with post from database

--- src/Controller/Website/PagesController.php
+++ src/Controller/Website/PagesController.php
@@ -2,20 +2,40 @@
 
 namespace App\Controller\Website;
 
+use App\Entity\Post;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Service\UploaderHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class PagesController extends AbstractController
 {
+    /**
+     * Homepage
+     *
+     * @param EntityManagerInterface $em
+     * @param Request $request
+     *
+     * @return Response
+     */
     public function homepage(EntityManagerInterface $em, Request $request)
     {
-        return $this->render('pages/homepage.html.twig');
+        /** @var Post[] $highlight */
+        $highlight = $em->getRepository(Post::class)->findBy([
+                'highlight' => true,
+                'active' => true,
+                'locale' => $request->getLocale()
+            ], ['published' => 'DESC'], 3);
+
+        return $this->render('pages/homepage.html.twig', [
+            'highlight' => $highlight
+        ]);
     }
 
     public function aboutme()
